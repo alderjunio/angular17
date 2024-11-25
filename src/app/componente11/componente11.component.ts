a5a46478837fa97e5443e1f143465bececfb2983
alderjunio <alder.junio@gmail.com>
2024-11-24 22:04:54 -0400

    criado o formulario e estilos css iniciais

---
+++ src/app/componente11/componente11.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente11',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente11.component.html',
+  styleUrl: './componente11.component.css'
+})
+export class Componente11Component {
+
+  nome:string;
+
+  idade:number = null;
+
+
+}
